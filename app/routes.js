ecfa8fd19f7658e954c30b2b19ae3eb143cf4a2b
Garima Kaila <garimakaila03@gmail.com>
2016-10-16 02:43:12 +0530

    testing CI & CD

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = {
 	create:function(app){
 			app.get("/",function(req,res,next){
-				res.render('home',{'name':'World'});
+				res.render('home',{'name':'Welcome to Mongo Mart'});
 			});
 
 
